64fc4726ae872caddd7d46c2bac3dc706c2c60c6
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2015-03-02 01:03:35 +0200

    Ignore files, move image, update build

--- tools/build.sh
+++ tools/build.sh
@@ -4,9 +4,13 @@
 # hosting repository to remove the /app from url.
 
 npm run build
+npm run build-less-info
+
+echo "Remove old files"
 
 # Copy built
 echo "Copying built files to ../busse-release.."
+echo "Copy main app"
 mkdir -p ../busse-release/scripts/libs/
 mkdir -p ../busse-release/images/
 mkdir -p ../busse-release/data/
@@ -15,6 +19,15 @@ cp app/index.html app/bundle.css app/bundle.js ../busse-release
 cp app/images/*.svg app/images/*.png ../busse-release/images/
 cp app/data/* ../busse-release/data/
 cp app/scripts/libs/* ../busse-release/scripts/libs/
+
+echo "Copy info page"
+mkdir -p ../busse-release/info/
+mkdir -p ../busse-release/info/images/
+mkdir -p ../busse-release/info/fonts/
+
+cp app/info/images/*.svg ../busse-release/info/images/
+cp app/info/fonts/* ../busse-release/info/fonts/
+cp app/info/*.js app/info/*.css app/info/index.html ../busse-release/info/
 
 echo "Adding git commit"
 git rev-parse HEAD > ../busse-release/commit
