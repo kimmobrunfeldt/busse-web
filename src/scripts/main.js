165b68c1735b66f4ca3a449902e5db6dde4e7fde
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2015-11-08 13:33:33 +0200

    Show whole world in the first load

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,14 +14,14 @@ const pageContainer = document.querySelector('#page');
 const mapLoader = createLoader('#Loader');
 
 createMapPage({
-    initialPosition: {latitude: 61.487881, longitude: 23.7810259},
+    initialPosition: {latitude: 0, longitude: 0},
     mapProvider: CONST.MAP_PROVIDER,
     mapBoxKey: CONST.MAP_BOX_KEY,
     mapBoxMapId: CONST.MAP_BOX_MAP_ID,
     hereMapsAppId: CONST.HERE_MAPS_APP_ID,
     hereMapsAppCode: CONST.HERE_MAPS_APP_CODE,
 
-    initialZoom: 5,
+    initialZoom: 2,
     zoomOnLocated: 16,
 
     hideMarkersAfterAmount: 20,
